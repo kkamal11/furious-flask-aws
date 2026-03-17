55d1b3064b9f55473e1b1d381f46b5d685564d39
kkamal11 <kishorkamal7091@gmail.com>
2026-03-17 06:23:17 +0530

    book-crud-op

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, flash
 from dotenv import load_dotenv
 
 from models.models import db, Book
@@ -8,6 +8,7 @@ load_dotenv()
 
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = "super-secret-key"
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
@@ -16,10 +17,51 @@ db.init_app(app)
 
 @app.route("/")
 def root():
-    return render_template("index.html")
+    books = Book.query.order_by(Book.id).all()
+    return render_template("index.html", books=books)
+
+
+@app.route("/add", methods=["POST"])
+def add_book():
+    title = request.form["title"]
+    author = request.form["author"]
+    price = request.form["price"]
+
+    new_book = Book(title, author, price)
+
+    db.session.add(new_book)
+    db.session.commit()
+
+    flash(f"Book {title} added successfully!", "success")
+
+    return redirect("/")
+
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    book = Book.query.get_or_404(id)
+    db.session.delete(book)
+    db.session.commit()
+    flash(f"Book {book.title} deleted successfully", "danger")
+    return redirect("/")
+
+
+@app.route("/update/<int:id>", methods=["POST"])
+def update_book(id):
+    book = Book.query.get_or_404(id)
+
+    book.title = request.form["title"]
+    book.author = request.form["author"]
+    book.price = request.form["price"]
+
+    db.session.commit()
+
+    flash(f"Book {book.title} updated successfully!", "info")
+
+    return redirect("/")
 
 
 if __name__ == "__main__":
     with app.app_context():
-        db.create_all()  # creates tables
+        db.create_all()  # creates tables  info warning
     app.run(debug=True)
